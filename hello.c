16938021b0a457a6980164c9a5fa2ca1769243ef
99002532 <72495925+99002532@users.noreply.github.com>
2020-10-09 09:20:43 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <hellomake.h>
+#include "hellomake.h"
 
 int main() {
   
- void myPrintHelloMake(){
+ void myPrintHelloMake()
+ {
  printf("Hello makefiles!\n");
 
  }
